906cd89a7441e06f7b12bc382bcaa0befb8d48f4
Michael Ruetz <michael.ruetz@volkswagen.de>
2017-02-24 14:11:06 +0100

    1 develop changes

--- TestGit/AppDelegate.swift
+++ TestGit/AppDelegate.swift
@@ -10,13 +10,13 @@ import UIKit  //Master//
 
 // Imp Changes // // //
 //
-  
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+  
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
